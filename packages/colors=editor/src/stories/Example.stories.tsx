ddf4e58e0bb09104a059a494a485df4280c31cbb
tigawanna <denniskinuthiawaweru@gmail.com>
2024-09-10 20:28:14 +0300

    uwu

---
+++ packages/colors=editor/src/stories/Example.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryFn } from "@storybook/react";
+
+import { Example } from "..";
+
+export default {
+	title: "Example",
+	component: Example,
+	argTypes: {},
+} as Meta<typeof Example>;
+
+const Template: StoryFn<typeof Example> = (args) => <Example {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+	text: "Clicked this many times:",
+};
---
+++ packages/colors=editor/tests/Example.test.tsx
@@ -0,0 +1,10 @@
+import renderer from "react-test-renderer";
+import { expect, it } from "vitest";
+import { Example } from "../src";
+
+it("renders correctly", () => {
+	const tree = renderer
+		.create(<Example text="Clicked this many times" />)
+		.toJSON();
+	expect(tree).toMatchSnapshot();
+});
